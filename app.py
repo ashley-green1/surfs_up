81b64417ba338b93dd5f44d5b115cd8481a84bce
Ashley Green <75928657+ashley-green1@users.noreply.github.com>
2021-02-28 19:27:45 -0600

    Update

--- app.py
+++ app.py
@@ -1,12 +1,66 @@
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 # Import Flask dependency
 from flask import Flask
+from flask import jsonify
 
-# Create a new Flask App Instance
+# Prepare the database file to be connected to later on
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Create a Flask application
 app = Flask(__name__)
 
-# Create Flask Routes
-@app.route('/')
+#Create app route
+@app.route("/")
 
 # Create a function
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+# Create Precipitation Analysis route
+@app.route("/api/v1.0/precipitation")
+
+# Create precipitation function
+def precipitation():
+    
+    # Calculate the date one year from the last date in data set.
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >=prev_year).all()
+
+    # Format results into a JSON structure
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
